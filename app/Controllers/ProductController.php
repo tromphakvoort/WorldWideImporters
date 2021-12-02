8bcda8eb623b7676a9ba90653fd03bcaa4677d8a
Tromp Hakvoort <tromphakvoort@outlook.com>
2021-12-02 12:23:38 +0100

    Fix: Add function to model instead of controller

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -13,15 +13,4 @@ class ProductController {
 
         require_once APP_ROOT . '/views/product.php';
     }
-
-    public function read(int $id)
-    {
-        $this->title = 'My first Product';
-        $this->description = 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ';
-        $this->price = 2.56;
-        $this->sku = 'MVC-SP-PHP-01';
-        $this->image = 'https://via.placeholder.com/150';
-
-        return $this;
-    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -115,10 +115,21 @@ class Product {
         //
     }
 
-    public function read(int $id) {
-        //
-    }
+//    public function read(int $id) {
+//        //
+//    }
 
+    // TEST function, function above needs Database connection!
+    public function read(int $id)
+    {
+        $this->title = 'My first Product';
+        $this->description = 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ';
+        $this->price = 2.56;
+        $this->sku = 'MVC-SP-PHP-01';
+        $this->image = 'https://via.placeholder.com/150';
+
+        return $this;
+    }
     public function update(int $id, array $data) {
         //
     }
