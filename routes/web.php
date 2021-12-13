6461a0f6ed15163470ffa3a420dc9c0690fca607
Tromp Hakvoort <tromphakvoort@outlook.com>
2021-12-13 20:03:18 +0100

    Feat: Add get,post route collections

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,20 @@ use Symfony\Component\Routing\RouteCollection;
 // routes system
 $routes = new RouteCollection();
 
-$routes->add('product', new Route(constant('URL_SUBFOLDER') . '/product/{id}', array('controller' => 'ProductController', 'method' => 'showAction'), array('id' => '[0-9]+')));
+// GET routes
+$httpCollection = new RouteCollection();
+$httpCollection->add('product', new Route(constant('URL_SUBFOLDER') . '/product/{id}', array('controller' => 'ProductController', 'method' => 'showAction'), array('id' => '[0-9]+')));
+$httpCollection->add('homepage', new Route(constant('URL_SUBFOLDER') . '/', array('controller' => 'PageController', 'method' => 'indexAction'), array()));
 
-$routes->add('homepage', new Route(constant('URL_SUBFOLDER') . '/', array('controller' => 'PageController', 'method' => 'indexAction'), array()));
+// Set all routes to get method
+$httpCollection->setMethods(array('GET'));
+
+// POST methods
+$postCollection = new RouteCollection();
+
+// set all routes to post method
+$postCollection->setMethods(array('POST'));
+
+// Add collections to main route collection
+$routes->addCollection($httpCollection);
+$routes->addCollection($postCollection);
