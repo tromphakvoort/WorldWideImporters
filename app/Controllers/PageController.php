b1eae56626276ba2b9fe800f81fe93586f193f1d
frederikvos <95341650+frederikvos@users.noreply.github.com>
2021-12-18 20:47:29 +0100

    Update PageController.php

--- app/Controllers/PageController.php
+++ app/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\Product;
+use App\Models\Categories;
 use Symfony\Component\Routing\RouteCollection;
 
 class PageController
@@ -10,6 +11,9 @@ class PageController
     // Homepage action
     public function indexAction(RouteCollection $routes) {
         $routeToProduct = str_replace('{id}', 1, $routes->get('product')->getPath());
+        &&
+        $routeToCategory = str_replace('{id}', 1, $routes->get('Categories')->getPath());
+
 
         require_once APP_ROOT . '/views/home.php';
     }
