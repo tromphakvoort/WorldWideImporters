415f041692d725014ea777ba53042ee80b60c1e7
frederikvos <95341650+frederikvos@users.noreply.github.com>
2022-01-06 22:24:24 +0100

    Update category1.php

--- views/category1.php
+++ views/category1.php
@@ -14,23 +14,28 @@ include("../templates/header.php");?>
 // select products
     $db = mysqli_select_db($connection, "webshop_wwi") or die("Error connecting to database" . mysqli_error());
 
-    $sql = mysqli_query($connection, "SELECT p.product_name, p.description, p.price_amount, p.stock, p.price_currency, p.price_precision, c.category_name FROM products p
-                                            JOIN productcategories pc ON p.id = pc.fk_prodcat_product_id
-                                            JOIN categories c ON pc.fk_prodcat_cat_id = c.id
-                                            WHERE c.id = 1 ");
+  //  $sql = mysqli_query($connection, "SELECT p.product_name, p.description, p.price_amount, p.stock, p.price_currency, p.price_precision, c.category_name FROM products p
+   //                                         JOIN productcategories pc ON p.id = pc.fk_prodcat_product_id
+    //                                        JOIN categories c ON pc.fk_prodcat_cat_id = c.id
+     //                                       WHERE c.id = 1 ");
 
-    $query="SELECT p.product_name, p.description, p.price_amount, p.stock, p.price_currency, p.price_precision, c.category_name FROM products p
-                                            JOIN productcategories pc ON p.id = pc.fk_prodcat_product_id
-                                            JOIN categories c ON pc.fk_prodcat_cat_id = c.id
-                                            WHERE c.id = 1 ";
+ //   $query="SELECT p.id, p.product_name, p.description, p.price_amount, p.stock, p.price_currency, p.price_precision, c.category_name FROM products p
+ //                                           JOIN productcategories pc ON p.id = pc.fk_prodcat_product_id
+ //                                           JOIN categories c ON pc.fk_prodcat_cat_id = c.id
+ //                                           WHERE c.id = 1 ";
+     $query="SELECT p.id, p.product_name  FROM products p";
 
-    $result = mysqli_query($connection, $query);
+    $results = mysqli_query($connection, $query);
+//temporarily deactivated
+ //   while ($row = mysqli_fetch_assoc($results)) {
+ //       printf ("<table><tr><th>%s</th></tr> <tr></tr><td>%s</td></tr> <tr><td> %s</td></tr> <tr>><td>%s</td></tr> <tr><td>%s</td></tr> <tr><td>%s</td></tr> </table>", $row["product_name"], $row["description"], $row["price_amount"], $row["stock"], $row["price_currency"], $row["category_name"]);
+ //   };
 
-    while ($row = mysqli_fetch_assoc($result)) {
-        printf ("<table><tr><th>%s</th></tr> <tr></tr><td>%s</td></tr> <tr><td> %s</td></tr> <tr>><td>%s</td></tr> <tr><td>%s</td></tr> <tr><td>%s</td></tr> </table>", $row["product_name"], $row["description"], $row["price_amount"], $row["stock"], $row["price_currency"], $row["category_name"]);
-    };
+    foreach($results as $result)
+
+
 ?>
-<div>
+<div 'margin-left: 10%; margin-right: 10%'>
     <span>
     <a href="product/1" target="_blank">
             <img style='height: 25%; width: 25%; object-fit: contain' src='public\assets\Products\category1\melkchocolade.jpg' />
