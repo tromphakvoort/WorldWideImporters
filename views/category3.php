df3ccb037e1bdc1793e0a0528ea642262c1c1868
Wesleyvw94 <wesley.van.wieringen@windesheim.nl>
2022-01-07 18:01:37 +0100

    Added images and edited cat 2 and 3 + add to cart button on products

--- views/category3.php
+++ views/category3.php
@@ -1,31 +1,45 @@
-<?php $title = "Melkchocolade | World Wide Importers";
-include("../templates/header.php");
-//mySQLi information
-$db_host = "10.10.1.3";
-$db_username = "admin";
-$db_password = "Ayqy5ar988lFbefJ8wzo";
+<?php $title = "Witte chocolade | World Wide Importers";
+include("../templates/header.php");?>
 
-// db connection
-$connection = mysqli_connect($db_host, $db_username, $db_password) or die("Error connecting to database" . mysqli_error());
+<body>
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
 
-// select products
-$db = mysqli_select_db($connection, "webshop_wwi") or die("Error connecting to database" . mysqli_error());
+                <?php
+                //mySQLi information
+                $db_host = "10.10.1.3";
+                $db_username = "admin";
+                $db_password = "Ayqy5ar988lFbefJ8wzo";
 
-$sql = mysqli_query($connection, "SELECT p.product_name, p.description, p.price_amount, p.stock, p.price_currency, p.price_precision, c.category_name FROM products p
-                                            JOIN productcategories pc ON p.id = pc.fk_prodcat_product_id
-                                            JOIN categories c ON pc.fk_prodcat_cat_id = c.id
-                                            WHERE c.id = 3 ");
+                // db connection
+                $connection = mysqli_connect($db_host, $db_username, $db_password) or die("Error connecting to database" . mysqli_error());
 
-$query="SELECT p.product_name, p.description, p.price_amount, p.stock, p.price_currency, p.price_precision, c.category_name FROM products p
-                                            JOIN productcategories pc ON p.id = pc.fk_prodcat_product_id
-                                            JOIN categories c ON pc.fk_prodcat_cat_id = c.id
-                                            WHERE c.id = 3 ";
+                // select products
+                $db = mysqli_select_db($connection, "webshop_wwi") or die("Error connecting to database" . mysqli_error());
 
-$result = mysqli_query($connection, $query);
+                // Query for category 3
+                $results = mysqli_query($connection, "SELECT p.id, product_name, file_location, filename, mimetype FROM products p JOIN productcategories pc ON p.id=fk_prodcat_product_id JOIN attachments a ON fk_att_product_id=p.id WHERE fk_prodcat_cat_id=3");
 
-while ($row = mysqli_fetch_assoc($result)) {
-    printf ("<table><tr><th>%s</th></tr> <tr></tr><td>%s</td></tr> <tr><td> %s</td></tr> <tr>><td>%s</td></tr> <tr><td>%s</td></tr> <tr><td>%s</td></tr> </table>", $row["product_name"], $row["description"], $row["price_amount"], $row["stock"], $row["price_currency"], $row["category_name"]);
-}
+                // Loop through products and display them
+                if ($results->num_rows > 0) {
+                    while ($i = $results->fetch_assoc()) {
+                        ?> <div class="card box-shadow">
+                            <div class="vertical-center"> <?php echo $i["product_name"];
+                                $imagelocation = $i["file_location"]. $i["filename"] . $i["mimetype"];
+                                ?></div> <a href=product/<?php echo $i["id"] ?> target="_blank">
+                                <img class="card-img-top" style='height: 300px; width: 300px; object-fit: contain' src="<?php echo $imagelocation; ?>" />
+                            </a>
+                        </div>
+                        <?php echo "<br>";
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
 
-
-include("../templates/footer.php");
+</body>
+<?php include("../templates/footer.php");?>
