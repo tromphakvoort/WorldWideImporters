9038f2359730c63bd4d71b8d3279092283e279de
Tromp Hakvoort <tromphakvoort@outlook.com>
2022-01-19 21:45:46 +0100

    Fix: Made footer a little bit smaller

--- templates/footer.php
+++ templates/footer.php
@@ -36,44 +36,7 @@
                 ></a>
         </section>
         <!-- Section: Social media -->
-
-        <!-- Section: Form -->
-        <section class="">
-            <form action="">
-                <!--Grid row-->
-                <div class="row d-flex justify-content-center">
-                    <!--Grid column-->
-                    <div class="col-auto">
-                        <p class="pt-2">
-                            <strong>Schrijf je in voor onze nieuwsbrief</strong>
-                        </p>
-                    </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-md-4 col-8">
-                        <!-- Email input -->
-                        <div class="form-outline form-white mb-4">
-                            <input type="email" id="form5Example21" class="form-control" />
-                            <label class="form-label" for="form5Example21">E-mailadres</label>
-                        </div>
-                    </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-auto">
-                        <!-- Submit button -->
-                        <button type="submit" class="btn btn-outline-light mb-4">
-                            Abonneren
-                        </button>
-                    </div>
-                    <!--Grid column-->
-                </div>
-                <!--Grid row-->
-            </form>
-        </section>
-        <!-- Section: Form -->
-
+        
         <!-- Section: Text -->
         <section class="mb-4">
             <p>
