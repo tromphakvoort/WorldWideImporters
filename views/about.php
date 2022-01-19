6db2a05681db54be39c37eeb1d12c5b91aefa027
Wesleyvw94 <wesley.van.wieringen@windesheim.nl>
2022-01-19 22:05:42 +0100

    Filled About Us page

--- views/about.php
+++ views/about.php
@@ -1,7 +1,15 @@
 <?php $title = "About | World Wide Importers";
 include("../templates/header.php"); ?>
-<div>
+<div class="container">
     <h1>Over ons</h1>
+
+
+<p>World Wide World Importers is een importeur en groothandel die producten levert aan verschillende warenhuizen en supermarkten in de Verenigde Staten. Ook levert WWI producten door aan weer andere groothandels. Incidenteel verkoopt WWI producten rechtstreeks aan consumenten. WWI werkt met een groot netwerk aan vertegenwoordigers die het land doortrekken om hun producten in de markt te krijgen. WWI heeft ambities om hun activiteiten ook in Europa op te starten vanuit een nieuw hoofdkantoor in Amsterdam, maar de aandeelhouders hebben de opstart steeds tegengehouden omdat ze het een te groot risico vinden om investeringen te doen terwijl belangrijke klanten zijn omgevallen door de hevige concurrentie van online aanbieders.
+</p>
+<p>
+    “Wij zijn WWI. We zijn trots op onze producten en geloven in kwaliteit. In ieder huis vindt een product van WWI zijn thuis. Ondanks onze grootte zien klanten ons als kleinschalig en betrokken. Persoonlijk klantcontact staat bij ons hoog in het vaandel.”
+</p>
+   <img src="https://media.istockphoto.com/photos/teamwork-picture-id874833532?k=6&m=874833532&s=170667a&w=0&h=KKWtb8W84n4IRGSYGnfSjRIyU_zLwl-1ewsfCLRAL20=">
 </div>
 
 <?php include("../templates/footer.php"); ?>
