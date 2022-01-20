8b0aaee2dc8c324c5d0eea2c7cc746c515ed3e17
frederikvos <95341650+frederikvos@users.noreply.github.com>
2022-01-20 20:41:39 +0100

    Update contact.php

--- views/contact.php
+++ views/contact.php
@@ -3,15 +3,25 @@ include("../templates/header.php"); ?>
 <div class="container">
 <div>
     <h1>Contact</h1>
-</div>
-    <p>How Should a Contact Page Look? Before we take a look at some great contact us page examples, we wanted to talk about how a contact page should look. There are some key contact us page best practices for you to consider when building your own.
-    </p><p>    Here are some tips to keep in mind when thinking about how your contact page should look:    </p>
-<ul>
-    <li> Don’t Use a Distracting Sidebar: Work on not crowding your page with things that distract from what you want your visitors to do. Keep it focused on the contact form. </li>
-    <li> Limit Form Fields: Make sure that your contact form only asks for the important details. Forms with tons of required fields asking for things like addresses and phone numbers can make conversion rates suffer greatly. </li>
-    <li> Represent Your Brand: The first contact is the beginning of a relationship with your visitor and your brand. You contact us page design and style will be part of your lead’s first impression of your small business. </li>
-    <li> Write Details About What You Want: When someone has landed on your contact us page, they need you to write instructions on what you expect them to do. </li>
-</ul>
-    <img src="https://i.stack.imgur.com/ctmJZ.png" width="600px" height="400px">
+    <p>Heb je een vraag of opmerking voor ons? Neem dan contact op via onderstaand formulier:</p>
+
+    <form>
+        <div class="form-group">
+            <label for="FormControlInput1">E-mailadres:</label>
+            <input type="email" class="form-control" id="FormControlInput1" placeholder="naam@voorbeeld.nl">
+        </div>
+        <div class="form-row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Voornaam:">
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Achternaam:">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="FormControlTextarea1">Vraag of opmerking:</label>
+            <textarea class="form-control" id="FormControlTextarea1" rows="3"></textarea>
+        </div>
+    </form>
 </div>
         <?php include("../templates/footer.php"); ?>
